a20b05629eafe1383c9186e21157b92680a563a2
Khinenw <deu07115@gmail.com>
2017-09-07 23:16:25 +0900

    Updated heroes of the storm

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import "babel-polyfill";
-import stormImage from "./resources/spin.png";
-import stormBack from "./resources/bg.png";
-import stormIsBest from "./resources/storm-best.mp3";
-import stormLove from "./resources/storm-love.mp3";
+const stormCDN = 'https://raw.githubusercontent.com/HelloWorld017/restaurance/master/resources/'
+const stormImage = stormCDN + 'spin.png';
+const stormBack = stormCDN + 'bg.png';
+const stormIsBest = stormCDN + 'storm-best.mp3';
+const stormLove = stormCDN + 'storm-love.mp3';
+const stormBG = stormCDN + 'hos.png';
 
 const $$ = document.querySelectorAll.bind(document);
 
-const stylesheet = document.createElement('style');
 const animationContent = [
 `@-webkit-keyframes sigong {
 	0% {
@@ -46,13 +47,28 @@ const animationContent = [
 	100% {
 		opacity: 1;
 	}
+}`,
+`@-webkit-keyframes sigong-loop {
+	0% {
+		transform: scale(0.7) translate(50px, -50px) rotate(0deg);
+	}
+
+	100% {
+		transform: scale(0.7) translate(50px, -50px) rotate(360deg);
+	}
+}`,
+
+`@keyframes sigong-loop {
+	0% {
+		transform: scale(0.7) translate(50px, -50px) rotate(0deg);
+	}
+
+	100% {
+		transform: scale(0.7) translate(50px, -50px) rotate(360deg);
+	}
 }`];
 
-const stormText = '♚♚히어로즈 오브 더 스☆톰♚♚가입시$$전원 카드팩☜☜뒷면100%증정※' +
-'♜월드오브 워크래프트♜펫 무료증정￥ 특정조건 §§디아블로3§§★공허의유산★초상화획득기회@@' +
-'즉시이동';
-const stormLink = 'https://battle.net/heroes';
-
+const stylesheet = document.createElement('style');
 document.head.appendChild(stylesheet);
 animationContent.forEach((rule) => {
 	try{
@@ -62,24 +78,34 @@ animationContent.forEach((rule) => {
 	}
 });
 
+const stormText = '♚♚히어로즈 오브 더 스☆톰♚♚가입시$$전원 카드팩☜☜뒷면100%증정※' +
+'♜월드오브 워크래프트♜펫 무료증정￥ 특정조건 §§디아블로3§§★공허의유산★초상화획득기회@@' +
+'즉시이동';
+const stormLink = 'https://battle.net/heroes';
+
 const sigong = (x, y, elem, duration) => {
 	const elemBox = elem.getBoundingClientRect();
 
 	const xDisp = x - (elemBox.left + elemBox.right) / 2;
 	const yDisp = y - (elemBox.top + elemBox.bottom) / 2;
 
-	elem.style.transition = `transform ${Math.random() * duration / 3 + duration * 2 / 3 - 2}s ease 2s`;
+	elem.style.transitionProperty = `transform`;
 	elem.style.position = 'relative';
 	elem.style.zIndex = 2;
 	elem.style.transform =
 		`translate(${xDisp}px, ${yDisp}px)` +
-		`rotateX(${duration * (Math.random() * 180 + 20)}deg)` +
-		`rotateY(${duration * (Math.random() * 180 + 20)}deg)` +
-		`rotateZ(${duration * (Math.random() * 180 + 20)}deg)` +
+		`rotateX(${duration * (Math.random() * 30 + 20)}deg)` +
+		`rotateY(${duration * (Math.random() * 30 + 20)}deg)` +
+		`rotateZ(${duration * (Math.random() * 30 + 20)}deg)` +
 		`scale(0)`;
+
+	elem.style.transitionTimingFunction = `cubic-bezier(${Math.random()},${Math.random()},${Math.random()},${Math.random()})`;
+	let length = Math.random() * duration / 3 + duration * 2 / 3 - 2;
+	elem.style.transitionDuration = length + 's';
+	elem.style.transitionDelay = (Math.random() * (duration - length - 2) + 2) + 's';
 };
 
-const restaurance = async ({x, y, width, height} = {}, elems, duration) => {
+const restaurance = async ({x, y, width, height}, elems, duration) => {
 	if(width === undefined) width = 150;
 	if(height === undefined) height = 150;
 	if(x === undefined) x = (window.innerWidth - width) / 2;
@@ -91,6 +117,8 @@ const restaurance = async ({x, y, width, height} = {}, elems, duration) => {
 
 	const storm = document.createElement('img');
 	const storm2 = document.createElement('img');
+	const bigPicture = document.createElement('img');
+
 	const setStorm = (storm) => {
 		storm.style.position = 'fixed';
 		storm.style.left = `${x}px`;
@@ -113,14 +141,22 @@ const restaurance = async ({x, y, width, height} = {}, elems, duration) => {
 	body.style.animationDuration = '.2s';
 	body.style.animationIterationCount = `${duration / 0.2}`;
 
+	body.appendChild(bigPicture);
 	body.appendChild(storm2);
 	body.appendChild(storm);
+
+	bigPicture.style.position = 'fixed';
+	bigPicture.style.left = '50vw';
+	bigPicture.style.top = '50vh';
+	bigPicture.style.transform = 'translate(-50%, -50%)';
+	bigPicture.style.opacity = 0;
 
 	const centerX = x + width / 2;
 	const centerY = y + height / 2;
 
 	storm.src = stormImage;
 	storm2.src = stormBack;
+	bigPicture.src = stormBG;
 
 	const extraElems = [];
 
@@ -138,16 +174,21 @@ const restaurance = async ({x, y, width, height} = {}, elems, duration) => {
 	textView.innerText = stormText;
 	textView.style.opacity = '0';
 	textView.style.position = 'fixed';
-	textView.style.top = '50vh';
+	textView.style.top = 'calc(50vh + 100px)';
 	textView.style.left = '50vw';
 	//Kong-Kong Color
 	textView.style.background = '#222222';
 	textView.style.color = '#f0f1f2';
 	textView.style.transform = 'translate(-50%, -50%)';
-	textView.style.animationName = 'sigong-text';
-	textView.style.animationDuration = '.4s';
-	textView.style.animationDelay = `${duration - 0.4}s`;
-	textView.style.animationFillMode = 'forwards';
+
+	const setSigongText = (textView) => {
+		textView.style.animationName = 'sigong-text';
+		textView.style.animationDuration = '.4s';
+		textView.style.animationDelay = `${duration - 0.4}s`;
+		textView.style.animationFillMode = 'forwards';
+	};
+	setSigongText(textView);
+	setSigongText(bigPicture);
 
 	//Uther: Well Met!
 	const kidnap = document.createElement('a');
@@ -167,6 +208,24 @@ const restaurance = async ({x, y, width, height} = {}, elems, duration) => {
 	[textView, sib, sil].forEach((v) => document.body.appendChild(v));
 
 	console.log(`${stormText}${stormLink}`);
+
+	setTimeout(() => {
+		[storm, storm2].forEach((v) => {
+			v.style.transition = `transform 1s ease`;
+		});
+		storm.style.transform = `rotate(${duration * 360}deg) scale(0.7) translate(50px, -50px)`;
+		storm2.style.transform = `rotate(${(duration + 1) * 360}deg) scale(0.7) translate(50px, -50px)`;
+
+		setTimeout(() => {
+			storm.style.transform = '';
+			storm.style.transition = 'initial';
+			storm.style.animationName = 'sigong-loop';
+			storm.style.animationDuration = '4s';
+			storm.style.animationFillMode = 'forwards';
+			storm.style.animationTimingFunction = 'linear';
+			storm.style.animationIterationCount = 'infinite';
+		}, 1000);
+	}, duration * 1000);
 };
 
 module.exports = restaurance;
